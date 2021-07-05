c32262ab374d36f3a0ca541c34cd4ea52860789c
naka <mininaka013@gmail.com>
2021-07-05 15:52:34 -0300

    E

--- AccursedShields.cs
+++ AccursedShields.cs
@@ -1,20 +1,9 @@
-using Terraria;
 using Terraria.ModLoader;
 
 namespace AccursedShields
 {
     public class AccursedShields : Mod
 	{
-		internal static AccursedShields Instance { get; private set; }
-
-		public AccursedShields() => Instance = this;
-
-        public override void Unload()
-        {
-            if (!Main.dedServ)
-            {
-                Instance = null;
-            }
-        }
+        public static AccursedShields Instance => ModContent.GetInstance<AccursedShields>();
     }
 }
